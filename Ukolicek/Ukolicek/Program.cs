7f5ad14240db82f7d9a09aa56affae0312782903
Pepista <144339628+Pepista@users.noreply.github.com>
2026-01-05 09:12:33 +0100

    update

--- Ukolicek/Ukolicek/Program.cs
+++ Ukolicek/Ukolicek/Program.cs
@@ -69,3 +69,94 @@ class Program //Funkce (Vypočítá a vrátí)
     }
 }
 */
+/*
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        VypisCisla(5);
+    }
+
+    static void VypisCisla(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+/*
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int vysledek = Nasob(3, 4);
+        Console.WriteLine("Součin je: " + vysledek);
+    }
+
+    static int Nasob(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+*/
+/*
+ using System;
+
+class Program
+{
+    static void Main()
+    {
+        int cislo = 20;
+
+        ZvysHodnotou(cislo);
+        Console.WriteLine("Po předání hodnotou: " + cislo);
+
+        ZvysOdkazem(ref cislo);
+        Console.WriteLine("Po předání odkazem: " + cislo);
+    }
+
+    static void ZvysHodnotou(int x)
+    {
+        x += 10;
+    }
+
+    static void ZvysOdkazem(ref int x)
+    {
+        x += 10;
+    }
+}
+
+ */
+/*
+ using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Zadej svůj věk: ");
+        int vek = int.Parse(Console.ReadLine());
+
+        string vysledek = PopisVeku(vek);
+        Console.WriteLine("Výsledek: " + vysledek);
+    }
+
+    static string PopisVeku(int vek)
+    {
+        if (vek < 10)
+            return "Dítě";
+        else if (vek <= 17)
+            return "Školák";
+        else
+            return "Dospělý";
+    }
+}
+ */
